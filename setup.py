00f1f573b8e02b057959fb52bf0f9aa563dd8ea2
Michael Lange <michael.lange@imperial.ac.uk>
2017-02-01 07:25:31 +0000

    Setup: Add dependency and license to pip installs

--- setup.py
+++ setup.py
@@ -6,8 +6,13 @@ import numpy as np
 
 setup(name='parcels',
       version='0.0.1',
-      description="""Framework for Lagrangian tracking of virtual
+      long_description="Lagrangian particle tracking framework",
+      long_description="""Framework for Lagrangian tracking of virtual
       ocean particles in the petascale age.""",
+      url='http://www.oceanparcels.org',
       author="Imperial College London",
+      license='MIT',
       packages=find_packages(exclude=['docs', 'examples', 'indlude', 'scripts', 'tests']),
+      install_requires=['numpy', 'sympy', 'cgen', 'cachetools', 'enum34'],
+      test_requires=['pytest', 'flake8', 'pytest-ipynb'])
 )
